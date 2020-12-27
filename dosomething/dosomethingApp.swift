31951ae525eaadd2c68d05735b195ac43b17ec99
Storiwr <storiwr@users.noreply.github.com>
2020-12-27 14:53:05 -0500

    Initial Commit

---
+++ dosomething/dosomethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  dosomethingApp.swift
+//  dosomething
+//
+//  Created by Stephen on 12/27/20.
+//
+
+import SwiftUI
+
+@main
+struct dosomethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
