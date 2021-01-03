9f56387efb71d68d8c3bfbed43938c28bf93a5be
Storiwr <storiwr@users.noreply.github.com>
2021-01-03 18:43:55 -0500

    failed to get check box to actually change anything. Need to work on logic. Laid out rough plans for functions

---
+++ dosomething/Logic.swift
@@ -0,0 +1,27 @@
+//
+//  Logic.swift
+//  dosomething
+//
+//  Created by Stephen on 1/3/21.
+//
+
+import Foundation
+import Combine
+
+func markComplete() {
+    // item.isComplete toggle
+}
+
+func save() {
+    // some json parsing shit
+}
+
+func load() {
+    // some more json parsing shit
+}
+
+func doToday() {
+//    item.isToday toggle
+    // something something checking local time
+//    if 3am for each istoday == false
+}
